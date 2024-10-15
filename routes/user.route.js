36205bfc6ac8a2f2f1cf9bb8c52a4dc28e107f18
Vikas Bhat D <vikasdbhat@gmail.com>
2024-10-15 20:54:58 +0530

    changed http method for /logout

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,7 @@ const router=Router();
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
-router.route('/logout').get(verifyJWT,logoutUser)
+router.route('/logout').delete(verifyJWT,logoutUser)
 router.route("/changePassword").patch(verifyJWT,changePassword)
 router.route("/changeUsername").patch(verifyJWT,changeUsername)
 router.route("/changeEmail").patch(verifyJWT,changeEmail)
